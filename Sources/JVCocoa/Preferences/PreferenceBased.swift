1c2baf214a288fec9acdb9bec6225bc15da12532
TheMisifit68 <janverrept@me.com>
2021-04-11 19:47:57 +0200

    Preference handling supports key paths now

--- Sources/JVCocoa/Preferences/PreferenceBased.swift
+++ Sources/JVCocoa/Preferences/PreferenceBased.swift
@@ -12,18 +12,23 @@ public protocol PreferenceBased{
 	associatedtype ApplicationKey:StringRepresentable
 	associatedtype PreferenceKey:StringRepresentableEnum
 	associatedtype PreferenceValue:Any
-
+	
 	
 	var userDefaults:UserDefaults {get}
 	
 	var preferences:[ApplicationKey:PreferenceValue]{get}
-
-	func getPreference<T>(forKey key: PreferenceKey, secondaryKey: PreferenceKey?) -> T?
-
-	func setPreference<T>(_ value: T, forKey key: PreferenceKey, secondaryKey: PreferenceKey?)
-
-	func removePreference(forKey key: PreferenceKey, secondaryKey: PreferenceKey?)
 	
+	func getPreference<T>(forKey key: PreferenceKey) -> T?
+	func getPreference<T>(forKeyPath keyPath: PreferenceKey...) -> T?
+	func getPreference<T>(forKeyPath keyPath: [PreferenceKey]) -> T?
+	
+	func setPreference<T>(_ value: T, forKey key: PreferenceKey)
+	func setPreference<T>(_ value: T, forKeyPath keyPath: PreferenceKey...)
+	func setPreference<T>(_ value: T, forKeyPath keyPath: [PreferenceKey])
+	
+	func removePreference(forKey key: PreferenceKey)
+	func removePreference(forKeyPath keyPath: PreferenceKey...)
+	func removePreference(forKeyPath keyPath: [PreferenceKey])
 	
 }
 
@@ -33,38 +38,83 @@ public extension PreferenceBased {
 		UserDefaults.standard // Can be overridden by the conforming class if needed
 	}
 	
-	func getPreference<T>(forKey key: PreferenceKey, secondaryKey: PreferenceKey? = nil) -> T?{
-		
-		if let dictionaryKey = secondaryKey?.stringValue{
-			let dictionary = userDefaults.value(forKey: key.stringValue) as? [String:Any]
-			return dictionary?[dictionaryKey] as? T
-		}else{
-			return userDefaults.value(forKey: key.stringValue) as? T
-		}
-		
+	
+	// MARK: - Gettign Prefs
+	
+	func getPreference<T>(forKey key: PreferenceKey) -> T?{
+		return userDefaults.value(forKey: key.stringValue) as? T
 	}
-
-	func setPreference<T>(_ value: T, forKey key: PreferenceKey, secondaryKey: PreferenceKey? = nil){
-		
-		if let dictionaryKey = secondaryKey?.stringValue{
-			var dictionary = userDefaults.value(forKey: key.stringValue) as? [String:Any]
-			dictionary?[dictionaryKey] = value
-			userDefaults.setValue(dictionary, forKey: key.stringValue)
-		}else{
-			userDefaults.setValue(value, forKey: key.stringValue)
-		}
-		
+	
+	func getPreference<T>(forKeyPath keyPath: PreferenceKey...) -> T?{
+		return getPreference(forKeyPath: keyPath)
 	}
+	
+	func getPreference<T>(forKeyPath keyPath: [PreferenceKey]) -> T?{
 		
-	func removePreference(forKey key: PreferenceKey, secondaryKey: PreferenceKey? = nil){
+		let lastIndex = keyPath.count-1
+		var currentDictionary:[String : Any]?
 		
-		if let dictionaryKey = secondaryKey?.stringValue{
-			var dictionary = userDefaults.value(forKey: key.stringValue) as? [String:Any]
-			dictionary?.removeValue(forKey: dictionaryKey)
-			userDefaults.setValue(dictionary, forKey: key.stringValue)
-		}else{
-			userDefaults.removeObject(forKey: key.stringValue)
+		for (keyNumber, key) in keyPath.enumerated(){
+			
+			if keyNumber == 0 {
+				currentDictionary = userDefaults.dictionary(forKey: key.stringValue)
+			}else if keyNumber < lastIndex{
+				currentDictionary = currentDictionary?[key.stringValue] as? [String:Any]
+			}
+			
 		}
+		
+		return currentDictionary?[keyPath.last?.stringValue ?? ""] as? T
+	}
+	
+	// MARK: - Setting Prefs
+	
+	func setPreference<T>(_ value: T, forKey key: PreferenceKey){
+		userDefaults.setValue(value, forKey: key.stringValue)
+	}
+	
+	func setPreference<T>(_ value: T, forKeyPath keyPath: PreferenceKey...){
+		setPreference(value, forKeyPath: keyPath)
+	}
+	
+	func setPreference<T>(_ value: T, forKeyPath keyPath: [PreferenceKey]){
+		
+		let lastIndex = keyPath.count-1
+		var currentDictionary:[String : Any] = [:]
+		
+		for  (keyNumber, key) in keyPath.enumerated().reversed(){
+			
+			if keyNumber == lastIndex {
+				currentDictionary = [key.stringValue:value]
+			}else if keyNumber > 0{
+				currentDictionary = [key.stringValue : currentDictionary]
+			}
+			
+		}
+		
+		userDefaults.setValue(currentDictionary, forKey: keyPath.first?.stringValue ?? "")
+	}
+	
+	// MARK: - Removing Prefs
+	
+	func removePreference(forKey key: PreferenceKey){
+		userDefaults.removeObject(forKey: key.stringValue)
+	}
+	
+	func removePreference(forKeyPath keyPath: PreferenceKey...){
+		 removePreference(forKeyPath: keyPath)
+	}
+	
+	func removePreference(forKeyPath keyPath: [PreferenceKey]){
+		
+		var parentKey = keyPath
+		parentKey.removeLast()
+		
+		var parentDirectory:[String : Any]? = getPreference(forKeyPath: parentKey)
+		
+		parentDirectory?.removeValue(forKey: keyPath.last?.stringValue ?? "")
+		
+		setPreference(parentDirectory, forKeyPath: 	parentKey)
 	}
 	
 }
